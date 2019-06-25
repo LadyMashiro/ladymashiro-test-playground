1b514125cb12f6e530961df7f77b8e5c5a241dda
Lady Mashiro <33541195+LadyMashiro@users.noreply.github.com>
2019-06-25 10:17:08 +0200

    changed multiselect like on vue-multiselect doc

---
+++ src/components/Terminal.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="terminal">
+        <div id="head"> </div>
+        <div id="bar">
+            <div id="buttons"></div>
+        </div>
+        <div id="screen">
+            <p class="font">root@localhost:~$</p>
+            <p class="machiko"></p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Terminal',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+    #head {
+        font-family: 'Dank Mono';
+        color: #9E9EA1;
+        margin: auto;
+        padding: 9px;
+        font-size: 16px;
+        text-align: center;
+    }
+    #bar {
+        text-align: center;
+        width: 750px;
+        height: 25px;
+        background-color: #EDF1F5;
+        margin: 0 auto;
+        font-family: monospace;
+        padding: auto;
+        float: none;
+        border-radius: 6px 6px 0px 0px;
+    }
+    #buttons {
+        background: url(https://svgshare.com/i/DoU.svg);
+        background-repeat: no-repeat;
+        background-position: left center;
+        background-size: 80px;
+        width: auto;
+        height:25px;
+        margin-left: 6px;
+    }
+    #screen {
+        width: 749px;
+        height: 450px;
+        border-radius: 0px 0px 6px 6px;
+        margin: 0 auto;
+        background-color: #434443;
+        padding: 1px 0px 1px 1px;
+    }
+    .machiko {
+        background: url(https://svgshare.com/i/Dnh.svg);
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 116px;
+        width: 116px;
+        height: 116px;
+        margin: 36% 21px;
+        float: right;
+    }
+    .font {
+        color: #9E9EA1;
+        font-family: 'Dank Mono';
+        font-style: italic;
+        font-size: 15px;
+        text-align: left;
+        position: static;
+        padding-left: 16px;
+        background-color: #434443;
+    }
+
+
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,30 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import data from '../data/data';
+Vue.use(Vuex)
+export default new Vuex.Store({
+  state: {
+    headerFooter: data.headerFooter,
+    klinikFilter: data.klinikFilter,
+    kliniken: data.kliniken,
+    options: data.options
+  },
+  getters: {
+    filteredHeaderFooter: state => {
+      if (state.klinikFilter.length > 0) {
+        return state.headerFooter.filter(item => item.klinik === state.klinikFilter);
+      }
+      return state.headerFooter;
+    }
+  },
+  actions: {
+    setKlinikFilter ({ commit }, value) {
+      commit('SET_KLINIK_FILTER', value);
+    }
+  },
+  mutations: {
+    SET_KLINIK_FILTER(state, value) {
+      state.klinikFilter = value;
+    }
+  }
+});
